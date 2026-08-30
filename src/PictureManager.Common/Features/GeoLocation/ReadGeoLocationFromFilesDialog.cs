e82199ab454d788fd7ae63b394ac3024f0cf44b6
martin-holy <martin.holy@hotmail.com>
2026-08-30 19:35:01 +0200

    [U] ReadGeoLocationFromFilesDialog: XPlatformMetadata

--- src/PictureManager.Common/Features/GeoLocation/ReadGeoLocationFromFilesDialog.cs
+++ src/PictureManager.Common/Features/GeoLocation/ReadGeoLocationFromFilesDialog.cs
@@ -1,4 +1,5 @@
 ﻿using MH.UI.Dialogs;
+using MH.Utils.Imaging;
 using PictureManager.Common.Features.MediaItem;
 using PictureManager.Common.Features.MediaItem.Image;
 using System.Threading;
@@ -13,10 +14,31 @@ public sealed class ReadGeoLocationFromFilesDialog : ProgressDialog<ImageM> {
     _autoRun();
   }
 
-  protected override Task _do(ImageM item, CancellationToken token) {
+  private Task _oldDo(ImageM item, CancellationToken token) {
     _reportProgress(item.FileName);
     var mim = new MediaItemMetadata(item);
     MediaItemS.ReadMetadata(mim, true);
     return mim.Success ? mim.FindGeoLocation(false) : Task.CompletedTask;
+  }
+
+  protected override Task _do(ImageM item, CancellationToken token) {
+    if (!FF.XPlatformMetadata)
+      return _oldDo(item, token);
+
+    _reportProgress(item.FileName);
+
+    var mim = new MediaItemMetadata(item);
+    var metadata = new ImageMetadata(item.FilePath);
+    
+    mim.GeoNameId = ImageS.GetGeoNameId(metadata);
+
+    if (metadata.GpsCoordinate is { } gps) {
+      mim.Lat = gps.Latitude;
+      mim.Lng = gps.Longitude;
+
+      return mim.FindGeoLocation(false);
+    }
+
+    return Task.CompletedTask;
   }
 }
