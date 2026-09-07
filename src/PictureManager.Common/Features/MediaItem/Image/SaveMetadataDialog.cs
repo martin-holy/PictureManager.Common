d58d0ea57edbfeae6abb086f233b56682bff452a
martin-holy <martin.holy@hotmail.com>
2026-09-07 18:19:09 +0200

    [U] SaveMetadataDialog: Using new ImageMetadata

--- src/PictureManager.Common/Features/MediaItem/Image/SaveMetadataDialog.cs
+++ src/PictureManager.Common/Features/MediaItem/Image/SaveMetadataDialog.cs
@@ -7,33 +7,27 @@ namespace PictureManager.Common.Features.MediaItem.Image;
 
 public sealed class SaveMetadataDialog : ParallelProgressDialog<ImageM> {
   private readonly ImageS _imageS;
-  private readonly int _quality;
 
-  public SaveMetadataDialog(ImageM[] items, ImageS imageS, int quality) :
+  public SaveMetadataDialog(ImageM[] items, ImageS imageS) :
     base("Saving metadata to files...", MH.UI.Res.IconImage, items) {
     _imageS = imageS;
-    _quality = quality;
     _autoRun();
   }
 
   protected override Task _do(ImageM item, CancellationToken token) {
     _reportProgress(item.FilePath);
-
-    if (FF.XPlatformMetadata)
-      _imageS.TryWriteMetadata(item);
-    else
-      _imageS.TryWriteMetadata(item, _quality);
+    _imageS.TryWriteMetadata(item);
 
     return Task.CompletedTask;
   }
 
-  public static async Task Open(ImageM[] items, ImageS imageS, int quality) {
+  public static async Task Open(ImageM[] items, ImageS imageS) {
     if (await ShowAsync(new MessageDialog(
           "Save metadata to files",
           "Do you really want to save image metadata to {0} file{1}?".Plural(items.Length),
           MH.UI.Res.IconQuestion,
           true)) != 1) return;
 
-    await ShowAsync(new SaveMetadataDialog(items, imageS, quality));
+    await ShowAsync(new SaveMetadataDialog(items, imageS));
   }
 }
