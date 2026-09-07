c3e5de3d64331445f71c5aa5dda04c1ee59c369e
martin-holy <martin.holy@hotmail.com>
2026-09-07 18:18:27 +0200

    [U] ImageResizeDialog: Using new ImageMetadata

--- src/PictureManager.Common/Features/MediaItem/Image/ImageResizeDialog.cs
+++ src/PictureManager.Common/Features/MediaItem/Image/ImageResizeDialog.cs
@@ -1,7 +1,6 @@
 ﻿using MH.UI.Dialogs;
 using MH.Utils;
 using MH.Utils.BaseClasses;
-using MH.Utils.Imaging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -71,7 +70,7 @@ public sealed class ImageResizeDialog : ParallelProgressDialog<ImageM> {
         File.Delete(dest);
       }
 
-      ImagingU.ResizeJpg(image.FilePath, dest, _px, _preserveMetadata, _preserveThumbnail, Core.Settings.Common.JpegQuality);
+      ImageS.ResizeJpeg(image, dest, _px, _preserveMetadata, _preserveThumbnail, Core.Settings.Common.JpegQuality);
     }
     catch (Exception ex) {
       Log.Error(ex, image.FilePath);
